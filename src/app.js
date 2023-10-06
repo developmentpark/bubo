c8e83e50d301238a2a39b0352fb6b095e0993d1f
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-06 14:01:53 -0500

    refactor to split the app of the HTTP server

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
 import { App } from "octokit";
-import { createNodeMiddleware } from "@octokit/webhooks";
 import fs from "fs";
-import http from "http";
 
 const appId = process.env.APP_ID;
 const webhookSecret = process.env.WEBHOOK_SECRET;
 const privateKeyPath = process.env.PRIVATE_KEY_PATH;
 
-// This reads the contents of your private key file.
 const privateKey = fs.readFileSync(privateKeyPath, "utf8");
 
-// This creates a new instance of the Octokit App class.
 const app = new App({
   appId: appId,
   privateKey: privateKey,
@@ -19,7 +15,6 @@ const app = new App({
   },
 });
 
-// This defines the message that your app will post to pull requests.
 const messageForNewPRs =
   "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
 const messageForResolveAutoReview =
@@ -41,7 +36,6 @@ async function isApprovedToAutoReview({ octokit, payload }) {
   return reviewers.length === 0;
 }
 
-// This adds an event handler that your code will call later. When this event handler is called, it will log the event to the console. Then, it will use GitHub's REST API to add a comment to the pull request that triggered the event.
 async function handlePullRequestOpened({ octokit, payload }) {
   console.log(
     `Received a pull request event for #${payload.pull_request.number}`,
@@ -104,10 +98,8 @@ async function handlePullRequestOpened({ octokit, payload }) {
   }
 }
 
-// This sets up a webhook event listener. When your app receives a webhook event from GitHub with a `X-GitHub-Event` header value of `pull_request` and an `action` payload value of `opened`, it calls the `handlePullRequestOpened` event handler that is defined above.
 app.webhooks.on("pull_request.opened", handlePullRequestOpened);
 
-// This logs any errors that occur.
 app.webhooks.onError((error) => {
   if (error.name === "AggregateError") {
     console.error(`Error processing request: ${error.event}`);
@@ -116,25 +108,4 @@ app.webhooks.onError((error) => {
   }
 });
 
-// This determines where your server will listen.
-//
-// For local development, your server will listen to port 3000 on `localhost`. When you deploy your app, you will change these values. For more information, see "[Deploy your app](#deploy-your-app)."
-const port = 3000;
-const host = "localhost";
-const path = "/api/webhook";
-const localWebhookUrl = `http://${host}:${port}${path}`;
-
-// This sets up a middleware function to handle incoming webhook events.
-//
-// Octokit's `createNodeMiddleware` function takes care of generating this middleware function for you. The resulting middleware function will:
-//
-//    - Check the signature of the incoming webhook event to make sure that it matches your webhook secret. This verifies that the incoming webhook event is a valid GitHub event.
-//    - Parse the webhook event payload and identify the type of event.
-//    - Trigger the corresponding webhook event handler.
-const middleware = createNodeMiddleware(app.webhooks, { path });
-
-// This creates a Node.js server that listens for incoming HTTP requests (including webhook payloads from GitHub) on the specified port. When the server receives a request, it executes the `middleware` function that you defined earlier. Once the server is running, it logs messages to the console to indicate that it is listening.
-http.createServer(middleware).listen(port, () => {
-  console.log(`Server is listening for events at: ${localWebhookUrl}`);
-  console.log("Press Ctrl + C to quit.");
-});
+export { app };
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import http from "http";
+import { createNodeMiddleware } from "@octokit/webhooks";
+import { app } from "./app.js";
+
+const port = 3000;
+const host = "localhost";
+const path = "/api/webhook";
+const localWebhookUrl = `http://${host}:${port}${path}`;
+
+const middleware = createNodeMiddleware(app.webhooks, { path });
+
+http.createServer(middleware).listen(port, () => {
+  console.log(`Server is listening for events at: ${localWebhookUrl}`);
+  console.log("Press Ctrl + C to quit.");
+});
