f10cb3ed212cca09666d795dd38f823446d5fb42
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-10-06 22:34:08 -0500

    refactor

--- src/reviewService.js
+++ src/reviewService.js
@@ -1,11 +1,5 @@
 import OctokitService from "./octokitService.js";
-
-const messageForNewPRs =
-  "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
-const messageForResolveAutoReview =
-  "Great job! The review has been approved and successfully merged. Let's keep building together!";
-const messageForRejectAutoReview =
-  "Automatic review was not performed as a reviewer has already been manually assigned to the pull request. Manual review is underway.";
+import { messages } from "./messages.js";
 
 async function isApprovedToAutoReview(octokitService) {
   const reviewers = await octokitService.getReviewers();
@@ -15,12 +9,12 @@ async function isApprovedToAutoReview(octokitService) {
 async function performAutoReview({ octokit, payload }) {
   const octokitService = new OctokitService({ octokit, payload });
   try {
-    await octokitService.postComment(messageForNewPRs);
+    await octokitService.postComment(messages.NEW_PR);
     const isApproved = await isApprovedToAutoReview(octokitService);
     if (!isApproved) {
-      return await octokitService.postComment(messageForRejectAutoReview);
+      return await octokitService.postComment(messages.REJECT_AUTO_REVIEW);
     }
-    await octokitService.postReview(messageForResolveAutoReview);
+    await octokitService.postReview(messages.RESOLVE_AUTO_REVIEW);
     await octokitService.postMerge();
   } catch (error) {
     console.error(`Error: ${error.message}`);
